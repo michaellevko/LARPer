0d68271e609e2092ce05816fa5c578329297feb5
michaellevko <michaellevko@gmail.com>
2017-03-29 02:24:55 +0300

    Google Auth works. Need to fix Update Google Play Services popup.

--- app/src/main/java/com/example/user/larper/MainActivity.java
+++ app/src/main/java/com/example/user/larper/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.user.larper;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("TAG", "MainActivity");
+        String userName = this.getIntent().getExtras().getString(getApplicationContext()
+                .getResources().getString(R.string.account_display_name));
+
+        ((TextView)this.findViewById(R.id.loginSuccessful)).setText(userName);
+
+        /* Check Firebase for user profile
+         * If profile doesnt exist, call InitProfileFragment
+         * Else, pull user profile and call HomeTabFragment*/
     }
 }
--- app/src/main/java/com/example/user/larper/SignInActivity.java
+++ app/src/main/java/com/example/user/larper/SignInActivity.java
@@ -2,6 +2,7 @@ package com.example.user.larper;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.View;
@@ -11,10 +12,12 @@ import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
 
-public class SignInActivity extends FragmentActivity implements View.OnClickListener {
+public class SignInActivity extends FragmentActivity implements
+        GoogleApiClient.OnConnectionFailedListener {
 
     private static final String TAG = "SignInActivity";
     private static final int RC_SIGN_IN = 9001;
@@ -25,41 +28,31 @@ public class SignInActivity extends FragmentActivity implements View.OnClickList
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
-
+        Log.d("TAG", "GoogleSignInOptions");
         // Configure sign-in to request the user's ID, email address, and basic
         // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
                 .build();
-
+        Log.d("TAG", "GoogleSignInOptions");
         // Build a GoogleApiClient with access to the Google Sign-In API and the
         // options specified by gso.
         mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this /* FragmentActivity */, (GoogleApiClient.OnConnectionFailedListener) this /* OnConnectionFailedListener */)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
                 .build();
-
+        Log.d("TAG", "GoogleApiClient");
         // Set the dimensions of the sign-in button.
         SignInButton signInButton = (SignInButton) findViewById(R.id.sign_in_button);
         signInButton.setSize(SignInButton.SIZE_STANDARD);
-
-        findViewById(R.id.sign_in_button).setOnClickListener(this);
-    }
-
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.sign_in_button:
-                signIn();
-                break;
-            // ...
-        }
-    }
-
-    private void signIn() {
-        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
-        startActivityForResult(signInIntent, RC_SIGN_IN);
+        // Set sign in button listener to fetch google account details
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+                startActivityForResult(signInIntent, RC_SIGN_IN);
+            }
+        });
     }
 
     @Override
@@ -80,6 +73,8 @@ public class SignInActivity extends FragmentActivity implements View.OnClickList
             GoogleSignInAccount acct = result.getSignInAccount();
             //mStatusTextView.setText(getString(R.string.signed_in_fmt, acct.getDisplayName()));
             updateUI(true);
+            launchMainActivity(acct);
+
         } else {
             // Signed out, show unauthenticated UI.
             updateUI(false);
@@ -98,5 +93,16 @@ public class SignInActivity extends FragmentActivity implements View.OnClickList
         }
     }
 
+    private void launchMainActivity(GoogleSignInAccount acct){
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra(getApplicationContext().getResources().getString(R.string.account_display_name), acct.getDisplayName());
+        intent.putExtra(getApplicationContext().getResources().getString(R.string.account_id), acct.getId());
+        startActivity(intent);
+        this.finish();
+    }
 
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d(TAG, "onConnectionFailed:" + connectionResult);
+    }
 }
